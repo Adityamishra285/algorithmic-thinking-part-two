b48428c01a803c0e4899da7563f06a0955d762ad
Josh Alamillo <josh.alamillo@gmail.com>
2018-07-09 20:22:15 -0700

    Created app file

---
+++ week_2_app.py
@@ -0,0 +1,4 @@
+'''
+APPLICATION 3: Efficiency of hierarchical clustering and k-means clustering
+'''
+
--- week_2_project.py
+++ week_2_project.py
@@ -112,7 +112,6 @@ def get_nearest_center_index(cluster, center_positions):
 
   return nearest_center_index
 
-
 def kmeans_clustering(cluster_list, num_clusters, num_iterations):
 
   '''Given a list of cluster objects, number of iterations, and number of clusters required, create a new list of clusters using
@@ -151,6 +150,8 @@ def kmeans_clustering(cluster_list, num_clusters, num_iterations):
     #replace each center position with the new center position.
   return center_clusters
   #return the final cluster list
+
+
 
 
 
